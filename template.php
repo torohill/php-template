d5b5263c5d2a16ebe0f476600677e1d466b74c9b
Toro Hill <toro@karere.co.nz>
2013-02-04 22:48:47 +1300

    Fix whitespace.

--- template.php
+++ template.php
@@ -5,45 +5,45 @@
  */
 class Template{
 	// Filename of template.
-    protected $file;
+	protected $file;
 
 	//Template variables.
-    protected $vars = array();
+	protected $vars = array();
 
 
 	/*
 	 * Public methods
 	 */
-    public function __construct($file){
-        $this->file = $file;
-    }
-    public function __get($key){
-        return array_key_exists($this->vars, $key) ? $this->vars[$key] : NULL;
-    }
-    public function __set($key, $value){
-        $this->vars[$key] = $value;
-    }
+	public function __construct($file){
+		$this->file = $file;
+	}
+	public function __get($key){
+		return array_key_exists($this->vars, $key) ? $this->vars[$key] : NULL;
+	}
+	public function __set($key, $value){
+		$this->vars[$key] = $value;
+	}
 
 	/*
 	 * Set multiple template variables at the same time.
 	 * $value should be an associative array.
 	 */
-    public function set(array $values){
-        $this->vars = array_merge($this->vars, $values);
-    }
+	public function set(array $values){
+		$this->vars = array_merge($this->vars, $values);
+	}
 
 	/*
 	 * Execute the template with the current template variables and return the output.
 	 * Note that the template files can access $this if needed.
 	 */
-    public function execute(){
-        extract($this->vars);
-        ob_start();
-        include($this->file);
-        $content = ob_get_contents();
-        ob_end_clean();
-        return $content;
-    }
+	public function execute(){
+		extract($this->vars);
+		ob_start();
+		include($this->file);
+		$content = ob_get_contents();
+		ob_end_clean();
+		return $content;
+	}
 
 	/*
 	 * Execute a template and echo the results.
