7fca6b5c87b3e2166bd002916a79dd6694b347fd
Toro Hill <toro@karere.co.nz>
2013-02-06 22:36:19 +1300

    fix #1: Add support for pre-setting the template path and suffix.

--- template.php
+++ template.php
@@ -2,6 +2,9 @@
 /*
  * A very simple template class. 
  * Assign template variables as member variables then call execute or use the static render method.
+ *
+ * Limitations:
+ * 	- Can't have a template variable named $this as it conflicts with the reference to the current object.
  */
 class Template{
 	// Filename of template.
@@ -10,6 +13,11 @@ class Template{
 	//Template variables.
 	protected $vars = array();
 
+	//Default path to templates. Static so used for all template objects.
+	protected static $path = '';
+
+	//Default template suffix. Static so used for all template objects.
+	protected static $suffix = '';
 
 	/*
 	 * Public methods
@@ -25,6 +33,27 @@ class Template{
 	}
 
 	/*
+	 * Set the default base path for all templates.
+	 * Saves having to specify the path for every template.
+	 * Should contain a trailing slash as will not be modified before prepending.
+	 * Will be prepended to all template names regardless of whether they are relative or absolute.
+	 * Set path to empty string to unset the current path.
+	 */
+	public static function set_path($path){
+		self::$path = $path;
+	}
+
+	/*
+	 * Set the default file suffix for all templates.
+	 * Saves having to specify the suffix for every template.
+	 * Will be appended to all template names.
+	 * Set suffix to empty string to unset the current suffix.
+	 */
+	public static function set_suffix($suffix){
+		self::$suffix = $suffix;
+	}
+
+	/*
 	 * Set multiple template variables at the same time.
 	 * $value should be an associative array.
 	 */
@@ -34,12 +63,13 @@ class Template{
 
 	/*
 	 * Execute the template with the current template variables and return the output.
-	 * Note that the template files can access $this if needed.
+	 * Note that the template files can access $this if needed (but it's probably not good form).
+	 * Don't define any other variables as they will pollute the scope in the template file.
 	 */
 	public function execute(){
 		extract($this->vars);
 		ob_start();
-		include($this->file);
+		include(self::$path . $this->file . self::$suffix);
 		$content = ob_get_contents();
 		ob_end_clean();
 		return $content;
