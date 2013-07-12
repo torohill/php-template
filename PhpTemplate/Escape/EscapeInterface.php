05c17c28578643c24624bf889606adf9478d3c98
Toro Hill <toro@karere.co.nz>
2013-07-12 14:00:18 +1200

    Add documentation to escape interface and class.

--- PhpTemplate/Escape/EscapeInterface.php
+++ PhpTemplate/Escape/EscapeInterface.php
@@ -1,7 +1,21 @@
 <?php
-
+/**
+ * Escape Interface definition.
+ *
+ * The interface which all objects that are used for escaping content must implement.
+ *
+ * @author	Toro Hill
+ * @link	https://bitbucket.org/torohill/php-template/
+ * @license MIT
+ */
 namespace PhpTemplate\Escape;
 
 interface EscapeInterface{
+	/**
+	 * Escape a value and return the escaped value.
+	 *
+	 * @param	string	$value	The value to be escaped.
+	 * @return	string			Escaped value.
+	 */
 	public function escape($value);
 }
--- PhpTemplate/Escape/HtmlEntities.php
+++ PhpTemplate/Escape/HtmlEntities.php
@@ -1,8 +1,25 @@
 <?php
-
+/**
+ * PHP Template HTML Entitites class.
+ *
+ * Class for escaping html entities.
+ *
+ * @author	Toro Hill
+ * @link	https://bitbucket.org/torohill/php-template/
+ * @license MIT
+ */
 namespace PhpTemplate\Escape;
 
 class HtmlEntities implements EscapeInterface{
+	/**
+	 * Escape html entities in a string.
+	 *
+	 * Encodes single and double quotes, replaces any invalid code sequences with U+FFFD
+	 * and handles code as HTML5.
+	 *
+	 * @param	string	$value	The html string to be escaped.
+	 * @return	string			Escaped html input.
+	 */
 	public function escape($value){
 		return htmlentities($value, ENT_QUOTES | ENT_SUBSTITUTE | ENT_HTML5, 'UTF-8');
 	}
